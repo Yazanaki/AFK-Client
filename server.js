ddcbe84134c0d0bd3f9ef8e2211a59ce904d058a
demxn013 <korraelementalmc@gmail.com>
2026-05-20 10:51:52 +0200

    Update import path for getProfileForHost in server.js to reflect new directory structure

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const {
   sendFreshSmpQueueCommand,
 } = require("./botmanager");
 
-const { getProfileForHost } = require("./index");
+const { getProfileForHost } = require("./src/index");
 
 const app = express();
 app.use(express.json());
